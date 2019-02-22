0975c4d4f6ab8c42335e37cfcfa1d687e6ea6a16
Everton Ferreira <eferreira@viceri.com.br>
2019-02-22 17:35:06 -0300

    Refatoração do teste

--- StringCalculatorKataTest/StringCalculatorTest.cs
+++ StringCalculatorKataTest/StringCalculatorTest.cs
@@ -8,39 +8,26 @@ namespace StringCalculatorKataTest
         [Fact]
         public void Given_An_Empty_String_The_Add_Method_Must_Return_Zero()
         {
-            var stringCalculator = new StringCalculator();
-            var expected = 0;
-
-            Assert.Equal(expected, stringCalculator.Add(""));
+            string.Empty.ShouldCalculateTo(0);
         }
 
         [Fact]
         public void Given_An_Single_Number_The_Add_Method_Must_Return_The_Same_Number()
         {
-            var stringCalculator = new StringCalculator();
-            var expected = 1;
-
-            Assert.Equal(expected, stringCalculator.Add("1"));
+            "1".ShouldCalculateTo(1);
         }
 
         [Fact]
         public void Given_Two_Numbers_The_Add_Method_Must_Return_The_Sum_Of_Both_Numbers()
         {
-            var stringCalculator = new StringCalculator();
-            var expected = 3;
-
-            Assert.Equal(expected, stringCalculator.Add("1,2"));
+            "1,2".ShouldCalculateTo(3);
         }
 
         [Fact]
         public void Given_Multiple_Numbers_The_Add_Method_Must_Return_The_Sum_Of_All_Numbers()
         {
-            var stringCalculator = new StringCalculator();
-            var expected = 10;
-
-            Assert.Equal(expected, stringCalculator.Add("1,2,3,4"));
+            "1,2,3,4".ShouldCalculateTo(10);
         }
-
 
         [Fact]
         public void Add_Method_Must_Handle_New_Lines_Between_Numbers()
@@ -49,6 +36,15 @@ namespace StringCalculatorKataTest
             var expected = 10;
 
             Assert.Equal(expected, stringCalculator.Add("1\n2\n3\n4"));
+        }
+
+    }
+    internal static class TestHelper
+    {
+        public static void ShouldCalculateTo(this string input, int expected)
+        {
+        var item = new StringCalculator();
+        Assert.Equal(expected, item.Add(input));
         }
     }
 }
